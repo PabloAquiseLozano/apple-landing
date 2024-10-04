15e34556d576705798aa8ac391e6d27dc021ea10
noelMoriano <nmoriano26@gmail.com>
2024-10-03 23:22:21 -0500

    add data list

---
+++ src/data-list/products.js
@@ -0,0 +1,70 @@
+import {
+  AirPods4,
+  AirPodsMax,
+  AirPodsPro2,
+  AppleWatch,
+  AppleWatchUltra,
+  Iphone16,
+  Pro,
+} from "../images/index.js";
+
+export const products = [
+  {
+    img: Pro,
+    title: "iPhone 16 Pro",
+    subTitle: "Hello, Apple Intelligence",
+    description:
+      "Hello, Apple Intelligence Built for Apple Intelligence — personal, private, powerful. Camera Control, an easier way to quickly access camera tools. Stunning 4K 120 fps Dolby Vision video. A18 Pro chip. And a huge leap in battery life.",
+    backgroundCard: "black",
+    textColor: "white",
+  },
+  {
+    img: Iphone16,
+    title: "iPhone 16",
+    subTitle: "Hello, Apple Intelligence",
+    description:
+      "Built for Apple Intelligence to help you write, express yourself, and get things done effortlessly. Camera Control, an easier way to quickly access camera tools. And the custom-built A18 chip.",
+    backgroundCard: "white",
+    textColor: "black",
+  },
+  {
+    img: AppleWatch,
+    title: "Apple Watch Series 10",
+    description:
+      "Our thinnest watch with our biggest display. Invaluable health insights, including sleep apnea notifications. Tracking for your activity and workouts — with depth and water temperature. All in our fastest-charging watch ever.",
+    backgroundCard: "white",
+    textColor: "black",
+  },
+  {
+    img: AppleWatchUltra,
+    title: "Apple Watch Ultra 2",
+    description:
+      "The ultimate sports and adventure watch features a stunning new black titanium case. With connectivity, health, and safety features for the everyday. And the most accurate GPS in a sports watch.",
+    backgroundCard: "black",
+    textColor: "white",
+  },
+  {
+    img: AirPods4,
+    title: "AirPods 4",
+    description:
+      "Updated fit for all-day comfort. A totally transformed audio experience. And available with Active Noise Cancellation — a first for this open-ear design.",
+    backgroundCard: "white",
+    textColor: "black",
+  },
+  {
+    img: AirPodsPro2,
+    title: "AirPods Pro 2",
+    description:
+      "Coming this fall with a free software update, the world’s first all-in-one hearing health experience — test, aid, and help protect your hearing.",
+    backgroundCard: "black",
+    textColor: "white",
+  },
+  {
+    img: AirPodsMax,
+    title: "AirPods Max",
+    description:
+      "The ultimate over-ear personal listening experience. Now in fresh new colors with fast and easy USB‑C charging.",
+    backgroundCard: "white",
+    textColor: "black",
+  },
+];
